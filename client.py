8fd065093640886e871145c964c915e6fa30e2b4
Nikita <reymoro3@gmail.com>
2023-09-25 21:36:30 +0300

    Change mistakes

--- client.py
+++ client.py
@@ -1,8 +1,20 @@
 import requests
-
+#POST
 response = requests.post(url='http://127.0.0.1:5000/ads',json={
     'title': 'Hello World',
     'description': 'Selling the world again',
     'user_name': 'A Man who sold the world'})
+print(response.status_code)
+print(response.text)
+
+# GET
+response = requests.get('http://127.0.0.1:5000/ads/7')
+
+print(response.status_code)
+print(response.text)
+
+# DELETE
+response = requests.delete('http://127.0.0.1:5000/ads/5')
+
 print(response.status_code)
 print(response.text)
