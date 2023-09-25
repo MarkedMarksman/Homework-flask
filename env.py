780a1cd3a0a8f87a92bea17abdeb203d6347f8a4
Nikita <reymoro3@gmail.com>
2023-09-25 21:34:59 +0300

    Change mistake

--- env.py
+++ env.py
@@ -1,7 +1,7 @@
 import os
 
-PG_USER = os.getenv("PG_USER", "postgres")
-PG_PASSWORD = os.getenv("PG_PASSWORD", "Pereira20089O")
+PG_USER = os.getenv("PG_USER", "")
+PG_PASSWORD = os.getenv("PG_PASSWORD", "")
 PG_HOST = os.getenv("PG_HOST", "127.0.0.1")
 PG_PORT = int(os.getenv("PG_PORT", 5432))
 PG_DB = os.getenv("PG_DB", "ads")
